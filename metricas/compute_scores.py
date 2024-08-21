d8a5c5b3da0b3515425248465ec50027304a5148
NicolasIsla <nico.isla1@gmail.com>
2024-08-21 07:48:01 -0400

    metricas

---
+++ metricas/compute_scores.py
@@ -0,0 +1,54 @@
+import os
+import numpy as np
+import pandas as pd
+import glob
+import argparse
+from metricas.compute_perf import (
+    evaluate_predictions,
+    evaluate_multiple_pred_folders,
+    find_best_conf_threshold_and_plot,
+)
+
+def main(args):
+    ds_name = args.dataset
+    IMG_FOLDER = f"Data/{ds_name}/images/val"
+    gt_folder = f"Data/{ds_name}/labels/val"
+    imgs = glob.glob(f"{IMG_FOLDER}/*")
+    imgs.sort()
+
+    print(f"Total validation images: {len(imgs)}")
+
+    pred_folders = glob.glob(f"Data/test_preds/**/labels")
+    pred_folders.sort()
+    pred_folders = [f for f in pred_folders if f"{ds_name}" in f]
+
+    print(f"Total prediction folders: {len(pred_folders)}")
+
+    # Define a range of possible confidence threshold values
+    conf_thres_range = np.linspace(0.01, 0.50, 50)
+    results_df = evaluate_multiple_pred_folders(pred_folders, gt_folder, conf_thres_range)
+    results_df = results_df.sort_values(by="Best F1 Score", ascending=False)
+
+    print(results_df.head())
+
+    folder_name = results_df["Prediction Folder"].values[0]
+    pred_folder = f"Data/test_preds/{folder_name}/labels"
+
+    print(f"Evaluating best folder: {folder_name}")
+
+    # Example usage
+    best_conf_thres, best_f1_score, best_precision, best_recall = (
+        find_best_conf_threshold_and_plot(pred_folder, gt_folder, conf_thres_range, True)
+    )
+
+    print(
+        f"Best Confidence Threshold: {best_conf_thres}\nBest F1 Score: {best_f1_score}\nPrecision: {best_precision}\nRecall: {best_recall}"
+    )
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Evaluate prediction folders and find the best confidence threshold.")
+    parser.add_argument("--dataset", type=str, required=True, help="Dataset name, used to specify the folder structure.")
+    
+    args = parser.parse_args()
+    
+    main(args)
---
+++ metricas/run_predictions.py
@@ -0,0 +1,23 @@
+import subprocess
+import glob
+import argparse
+
+def main(data_directory, model_directory):
+    source = f"{data_directory}/images/val"
+    weights = glob.glob(f"{model_directory}/*")
+
+    print(f"Number of weight files found: {len(weights)}")
+
+    for weight in weights:
+        model_name = weight.split('/')[-1].split('.')[0]
+        cmd = f"yolo predict model={weight} iou=0.01 conf=0.01 source={source} save=False save_txt save_conf project={data_directory}/test_preds name={model_name}"
+        print(f"* Command:\n{cmd}")
+        subprocess.call(cmd, shell=True)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run YOLO predictions on validation set for multiple models.")
+    parser.add_argument('--data_directory', type=str, required=True, help='Path to the dataset directory.')
+    parser.add_argument('--model_directory', type=str, required=True, help='Path to the directory containing model weights.')
+
+    args = parser.parse_args()
+    main(args.data_directory, args.model_directory)
