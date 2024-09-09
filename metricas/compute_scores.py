3cf4fddf9454c51658f3016aa15b9bc72b4536af
NicolasIsla <nico.isla1@gmail.com>
2024-09-09 14:48:46 -0300

    metricas

--- metricas/compute_scores.py
+++ metricas/compute_scores.py
@@ -22,21 +22,21 @@ def main(args):
     
     
     
-    # # Obtener resultados de la evaluación de predicciones
-    # results_df = evaluate_multiple_pred_folders(pred_folder, gt_folder, conf_thres_range)
-    # results_df = results_df.sort_values(by="Best F1 Score", ascending=False)
+    # Obtener resultados de la evaluación de predicciones
+    results_df = evaluate_multiple_pred_folders(pred_folder, gt_folder, conf_thres_range)
+    results_df = results_df.sort_values(by="Best F1 Score", ascending=False)
 
-    # print(results_df)
+    print(results_df)
 
-    # Obtener los mejores valores utilizando la función proporcionada
-    for pred_foldera in pred_folder:
-        best_conf_thres, best_f1_score, best_precision, best_recall = (
-            find_best_conf_threshold_and_plot(pred_foldera, gt_folder, conf_thres_range, True)
-        )
+    # # Obtener los mejores valores utilizando la función proporcionada
+    # for pred_foldera in pred_folder:
+    #     best_conf_thres, best_f1_score, best_precision, best_recall = (
+    #         find_best_conf_threshold_and_plot(pred_foldera, gt_folder, conf_thres_range, True)
+    #     )
 
-        print(
-            f"Best Confidence Threshold: {best_conf_thres}\nBest F1 Score: {best_f1_score}\nPrecision: {best_precision}\nRecall: {best_recall}"
-        )
+    #     print(
+    #         f"Best Confidence Threshold: {best_conf_thres}\nBest F1 Score: {best_f1_score}\nPrecision: {best_precision}\nRecall: {best_recall}"
+    #     )
    
 
 if __name__ == "__main__":
