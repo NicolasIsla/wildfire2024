174ba1853488b8712acb6f44f7e875f44648b403
NicolasIsla <nico.isla1@gmail.com>
2024-09-09 22:32:51 -0300

    das

--- metricas/run_predictions_test.py
+++ metricas/run_predictions_test.py
@@ -3,14 +3,17 @@ import glob
 import argparse
 
 datasets_test =[
-    r"/data/nisla/2019a-smoke-full/DS/images/test/",
-    r"/data/nisla/AiForMankind/DS/images/test/",
-    # r"/data/nisla/total_Combine/DS/images/test/",
-    r"/data/nisla/SmokesFrames-2.4k/DS/images/test/",
-    # r"/data/nisla/Wilfire_2023/DS/images/test/",
-    r"/data/nisla/Nemo/DS/images/test/",
-    r"/data/nisla/DS_08_V1/DS/images/test/",
-    # r"/data/nisla/DS_08_V2/DS/images/test/"
+    # r"/data/nisla/2019a-smoke-full/DS/images/test/",
+    # r"/data/nisla/AiForMankind/DS/images/test/",
+    # # r"/data/nisla/total_Combine/DS/images/test/",
+    # r"/data/nisla/SmokesFrames-2.4k/DS/images/test/",
+    # # r"/data/nisla/Wilfire_2023/DS/images/test/",
+    # r"/data/nisla/Nemo/DS/images/test/",
+    # r"/data/nisla/DS_08_V1/DS/images/test/",
+    # # r"/data/nisla/DS_08_V2/DS/images/test/"
+    r"/data/nisla/NemoV2/Nemo_yolo/DS/images/test/"
+
+
 
 ]
 def main(model_directory):
