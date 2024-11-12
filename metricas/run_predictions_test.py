6fda3d58a9f9becca003ae246d4052b5bc18292e
Nicolas Isla <72005173+NicolasIsla@users.noreply.github.com>
2024-11-12 14:52:35 -0300

    Update run_predictions_test.py

--- metricas/run_predictions_test.py
+++ metricas/run_predictions_test.py
@@ -14,7 +14,8 @@ datasets_test =[
     # r"/data/nisla/NemoV2/Nemo_yolo/DS/images/test/"
     # r"/data/nisla/NemoV3/DS/images/test/",
     # r"/data/nisla/Smoke50v3/DS/images/test/",
-    r"/data/nisla/TestSmoke/testsmoke/images/test/",
+    # r"/data/nisla/TestSmoke/testsmoke/images/test/",
+    r"/data/nisla/TestSmokev2/testsmokev2/images/test_10/",
 
 ]
 def main(model_directory):
