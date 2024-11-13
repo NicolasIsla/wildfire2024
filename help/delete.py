ca7c3c70cdb9d21acee2e046337c024b5d627e83
NicolasIsla <nico.isla1@gmail.com>
2024-11-13 02:58:36 -0300

    as

--- help/delete.py
+++ help/delete.py
@@ -16,30 +16,30 @@ deleted_images_count = 0
 deleted_labels_count = 0
 
 for dataset_dir in datasets_directories:
-    for root, _, files in os.walk(dataset_dir):
-        print
-        for file in files:
-            file_path = os.path.join(root, file)
-            if file_path in images_to_delete:
-                # Eliminar la imagen
-                try:
-                    os.remove(file_path)
-                    deleted_images_count += 1
-                    print(f"Imagen eliminada: {file_path}")
-                except Exception as e:
-                    print(f"Error al eliminar imagen {file_path}: {e}")
+    for file_path in os.listdir(dataset_dir):
+        name = os.basename(file_path)
+            
+        
+        if name in images_to_delete:
+            # Eliminar la imagen
+            try:
+                os.remove(file_path)
+                deleted_images_count += 1
+                print(f"Imagen eliminada: {file_path}")
+            except Exception as e:
+                print(f"Error al eliminar imagen {file_path}: {e}")
 
-                # Buscar y eliminar la etiqueta correspondiente
-                label_path = os.path.splitext(file_path)[0] + '.txt'
-                # replace images with labels
-                label_path = label_path.replace('images', 'labels')
-                if os.path.exists(label_path):
-                    try:
-                        os.remove(label_path)
-                        deleted_labels_count += 1
-                        print(f"Etiqueta eliminada: {label_path}")
-                    except Exception as e:
-                        print(f"Error al eliminar etiqueta {label_path}: {e}")
+            # Buscar y eliminar la etiqueta correspondiente
+            label_path = os.path.splitext(file_path)[0] + '.txt'
+            # replace images with labels
+            label_path = label_path.replace('images', 'labels')
+            if os.path.exists(label_path):
+                try:
+                    os.remove(label_path)
+                    deleted_labels_count += 1
+                    print(f"Etiqueta eliminada: {label_path}")
+                except Exception as e:
+                    print(f"Error al eliminar etiqueta {label_path}: {e}")
 
 print(f"Proceso completado. Total de imágenes eliminadas: {deleted_images_count}.")
 print(f"Total de etiquetas eliminadas: {deleted_labels_count}.")
